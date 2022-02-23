7532c8509b495e7522aa43cb0687257ab9dbfe3b
AkatorMr <marck_sparkle@hotmail.com>
2022-02-23 14:49:51 -0300

    es mas o menos lo que quiero

--- DataExtractor/Program.cs
+++ DataExtractor/Program.cs
@@ -3,21 +3,50 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.IO;
+using System.Diagnostics;
 
 namespace DataExtractor
 {
     class Program
     {
+        private static Process process;
+
+        //exiftool -XPComment="new comment2" test2.jpg   
+
         static void Main(string[] args)
         {
             String execPath = AppDomain.CurrentDomain.BaseDirectory;
 
-            for(int i = 0; i<args.Length; i++)
+
+            string[] allfiles = Directory.GetFiles(execPath, "*", SearchOption.AllDirectories);
+            foreach (var file in allfiles)
             {
-                GetBinData(execPath, args[i]);
+                FileInfo info = new FileInfo(file);
+                // Do something with the Folder or just add them to a list via nameoflist.add();
+                String pa = info.Directory.ToString();
+                pa=pa.Replace(execPath,"");
+                Console.WriteLine("\\"+pa+"\\"+info.Name.ToString());
             }
+            
+            /* for (int i = 0; i < args.Length; i++)
+            {
+                AgregarNota(execPath + args[i], "Esta es una nota");
+
+                GetBinData(execPath, args[i]);
+            } */
 
             Console.WriteLine("Operación completa");
+        }
+
+        private static void AgregarNota(string v1, string v2)
+        {
+            Console.WriteLine(v1);
+            process = new Process();
+            process.StartInfo.FileName = "exiftool.exe";
+            process.StartInfo.Arguments = "-XPComment=\"" + v2 + "\" \"" + v1 + "\"";
+
+            process.Start();
+            process.WaitForExit();
         }
 
         private static void GetBinData(string execPath, string p)
@@ -27,25 +56,25 @@ namespace DataExtractor
 
 
 
-            FileStream fileStream = new FileStream(orgPath, FileMode.Open,FileAccess.Read);
+            FileStream fileStream = new FileStream(orgPath, FileMode.Open, FileAccess.Read);
 
             BinaryReader binReader = new BinaryReader(fileStream, Encoding.UTF8);
 
 
             BinaryWriter binWriter =
             new BinaryWriter(File.Open(binPath, FileMode.Create));
-            
-            
-            
+
+
+
 
             try
             {
-                byte ac=0, ant=0;
-                bool write = false,write2=false;
+                byte ac = 0, ant = 0;
+                bool write = false, write2 = false;
 
                 binWriter.Flush();
 
-                for (int i = 0; i < fileStream.Length; i++) 
+                for (int i = 0; i < fileStream.Length; i++)
                 {
                     ac = binReader.ReadByte();
 
@@ -53,8 +82,8 @@ namespace DataExtractor
                         write = ant == 0xFF && ac == 0xD9;
                     else
                         if (!write2)
-                            write2 = ant == 0x49 && ac == 0x52;
-                        else
+                        write2 = ant == 0x49 && ac == 0x52;
+                    else
                     {
                         //Write stream
                         //binWriter.Write(fileBytes); // just feed it the contents verbatim
@@ -69,20 +98,20 @@ namespace DataExtractor
 
                 binReader.Close();
                 fileStream.Close();
-            
-            
+
+
             }
             catch (EndOfStreamException eose)
             {
                 binReader.Close();
                 fileStream.Close();
-            
+
             }
             //binReader.ReadByte();
             //byte[] fileBytes = binReader.ReadBytes((int)fileStream.Length);
             //byte[] fileBytes = System.IO.File.ReadAllBytes(fileNameWithPath_); // this also works
 
-            
+
 
 
         }
